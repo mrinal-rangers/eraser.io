799a0aa15ee9b046f260a75d179ad2172a640cfe
Mrinal Deb <mrinal437a@gmail.com>
2024-04-25 18:27:33 +0530

    ladning page done: easy used HyperUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (<div>
-    <h1>hello</h1>
-    <Button>Click ME</Button>
+    <Header/>
+    <Hero/>
   </div>);
 }
