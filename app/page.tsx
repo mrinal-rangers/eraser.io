5c71ee633ea17ccc35b26116bb295ffcf0c2745e
Mrinal Deb <mrinal437a@gmail.com>
2024-04-25 17:23:07 +0530

    folder setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (<div>
+    <h1>hello</h1>
+    <Button>Click ME</Button>
+  </div>);
+}
